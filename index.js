c350d84e5b784803af5d0db354b23fb13996db2b
Jonathan Alvares <jonthan.alvares@yahoo.com>
2022-03-28 16:37:08 +0530

    Added html for heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.sendFile(process.cwd() + "/index.html");
 });
 
 app.post("/api/schedule-events", async (req, res) => {
